34bd62107726c8a9e03622f78a7cde64b02d5b81
atsoria <slamber@Miguels-Air.attlocal.net>
2024-04-01 23:06:11 -0500

    adding final update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,59 @@
-#Always begin by importing to get the pathways correct
+#Import to get the pathways correct
 import os
 import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
 outputpath = os.path.join('Analysis', 'results.txt')
-# Lists to store data
-
-with open(csvpath,"r") as csvfile: #3.2 activity 05  also see activity 08/09
+#store the header
+csv_header = None
+with open(csvpath,"r") as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
-    # Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
 #Variables
-    TotalMonths = 0
+    changes = []
+    total_months = 0
+    net_total = 0
+    previous_profit = None
+    max_change = -999999999
+    min_change = 999999999
+    max_date = ""
+    min_date = ""
+
 
     for row in csvreader:
-        TotalMonths += 1 
+        #Find total months
+        total_months += 1 
+        #Find net total of profit and losses
+        profit_loss = int(row[1])
+        net_total += profit_loss
+    
+        change = 0
+        if previous_profit is not None:
+            change = profit_loss - previous_profit
+            changes.append(change)
+        previous_profit = profit_loss
+        #Find greatest increase in profits and print the corresponding date
+        if max_change < change:
+            max_change = change
+            max_date = row[0]
+        #Find greatest decrease in profits and print the corresponding date
+        if min_change > change:
+            min_change = change
+            min_date = row[0]
+#Find avg change of profit and losses
+average_change = sum(changes)/len(changes)
 
 
 output = "Financial Analysis\n"
 output += "----------------------------\n"
-#The total number of months included in the dataset
-output += f"Total Months: {TotalMonths}\n"
-#The net total amount of "Profit/Losses" over the entire period
- # print(f"Total: "{Total})
-#The changes in "Profit/Losses" over the entire period, and then the average of those changes
- # print(f"Average Cahnge: "{AverageChange})
-#The greatest increase in profits (date and amount) over the entire period
- # print(f"Greatest Increase in Profits: "{Date} {ProfitIncrease})
-#The greatest decrease in profits (date and amount) over the entire period
- #print(f"Greatest Decrease in Profits: "{Date2} {ProfitDecrease})
+output += f"Total Months: {total_months}\n"
+output += f"Total: ${net_total}\n"
+output += f"Average Change: ${average_change:.2f}\n"
+output += f"Greatest Increase in Profits: {max_date} (${max_change})\n"
+output += f"Greatest Decrease in Profits: {min_date} (${min_change})\n"
+#Print the results 
 print(output)
+#Write to text file
 with open(outputpath, "w") as txtfile:
     txtfile.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,50 @@
+#Import to get the pathways correct
+import os
+import csv
 
+csvpath = os.path.join('Resources','election_data.csv')
+outputpath = os.path.join('Analysis', 'results.txt')
+# Store the header
+csv_header = None
+with open(csvpath,"r") as csvfile: 
+    csvreader = csv.reader(csvfile, delimiter=",") 
+    csv_header = next(csvreader)
+
+#Variables
+    
+    total_votes = 0
+    votes_dictionary = {}
+    for row in csvreader:
+        #Find total votes
+        total_votes += 1 
+        #Find candidates names
+        candidate = row[2]
+        #Add candidate to dictionary and add vote count
+        if candidate in votes_dictionary:
+            votes_dictionary[candidate] = votes_dictionary[candidate]+1 
+        else:
+            votes_dictionary[candidate] = 1
+
+
+output = "Election Results\n"
+output += "----------------------------\n"
+output += f"Total Votes: {total_votes}\n"
+output += "----------------------------\n"
+winner = ""
+winner_votes = 0
+#Find results for each candidate
+for candidate2 in votes_dictionary:
+    the_votes = votes_dictionary[candidate2]
+    output += f"{candidate2}: {100*(the_votes/total_votes):.3f}% ({the_votes})\n"
+    #Find the winner
+    if the_votes > winner_votes:
+        winner = candidate2
+        winner_votes = the_votes
+output += "----------------------------\n"
+output += f"Winner: {winner} \n"
+output += "----------------------------\n"
+#print results
+print(output)
+#write to text file
+with open(outputpath, "w") as txtfile:
+    txtfile.write(output)
